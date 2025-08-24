b1b661afa309c97b6e07419b246349739dbaddcd
Funkegrace <olanrewajufunke28@gmail.com>
2025-08-24 22:01:42 +0200

    added chart from chart.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
+import { CryptoOptionsComponent } from "./components/crypto-options/crypto-options.component";
+import { CryptoPriceChartComponent } from './components/crypto-price-chart/crypto-price-chart.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent, CryptoOptionsComponent, CryptoPriceChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
